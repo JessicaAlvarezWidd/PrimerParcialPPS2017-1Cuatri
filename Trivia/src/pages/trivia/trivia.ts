2e437ccf09853965961919798ebdd8be46e03003
JessicaAlvarezWidd <jessica.alvarezwidd@gmail.com>
2017-04-18 21:30:55 -0300

    About y TriviaPage se agrego la foto del programodor en la pag About. se agrego funcionalidad a la pag Trivia.

--- Trivia/src/pages/trivia/trivia.ts
+++ Trivia/src/pages/trivia/trivia.ts
@@ -16,14 +16,57 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 export class Trivia {
 
   items: FirebaseListObservable<any>;
+  ResUser: FirebaseListObservable<any>;
+  points = 0;
+  preguntasRespondidas:Array<any>=[];
+  cantPreguntas = 0;
+  preguntaActual = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,af: AngularFire) {
     this.items = af.database.list('/Trivia');
-    console.log(this.items);
+    //console.log(this.items);
+    this.ResUser = af.database.list('/ResUser');
+    this.items.subscribe(result => {this.cantPreguntas = result.length});
+    var numRandom = Math.floor(Math.random()*(this.cantPreguntas)+1);
+    this.preguntasRespondidas.push(numRandom);
+    this.preguntaActual = numRandom;
+    console.log(this.preguntasRespondidas);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Trivia');
+  }
+
+  Respuesta(ResSelect,clave)
+  {
+    var preguntaCorrecta = false;
+
+    this.items.subscribe(items => items.forEach(
+      item => {
+                    
+          if(item.$key == clave)
+          {          
+            
+              if(ResSelect == item.respuesta)
+              {
+                  preguntaCorrecta = true;
+                  this.points += 5;
+              
+              }
+          }
+      }
+
+      ));      
+      
+      do
+      {
+        var numRandom = Math.floor(Math.random()*(this.cantPreguntas)+1);       
+      }
+      while(this.preguntasRespondidas.indexOf(numRandom) != -1); 
+      this.preguntasRespondidas.push(numRandom); 
+      this.preguntaActual = numRandom;
+      console.log(this.preguntasRespondidas);
+      
   }
 
 }
